eb7d60b9304d6b4752910c494f4a3461177ec24d
willbooker350 <codeninjascna3@gmail.com>
2024-05-18 14:28:42 -0400

    step60

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,26 +5,76 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject title;
     private Spawner spawner;
+    private Vector2 screenBounds;
+    public GameObject splash;
+
+    [Header("Player")]
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
     // Start is called before the first frame update
 
-   private void Awake()
+    private void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
-        
+
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true); 
+        splash.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (!gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        }
+        else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+
+        void ResetGame()
         {
             spawner.active = true;
+            title.SetActive(false);
+            splash.SetActive(false);
+
+            player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+            gameStarted = true;
         }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y - 12))
+            {
+                Destroy(bombObject);
+            }
+            
+        }
+    }
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetActive(true);
     }
 }
