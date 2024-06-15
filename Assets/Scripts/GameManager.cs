053cc1b4d267d335f89f0b1ceabd80f4ef0bfaa3
Somethingfrog <140408834+CloudiiPlayz@users.noreply.github.com>
2024-06-15 12:06:23 -0400

    done :)
    i finished :)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,13 @@ public class GameManager : MonoBehaviour
     public int pointsWorth = 1;
     private int score;
 
+    private int bestScore = 0;
+    public TMP_Text bestScoreText;
+    private bool beatBestScore;
+
+    public Color normalColor;
+    public Color bestScoreColor;
+
     private bool smokeCleared = true;
     // Start is called before the first frame update
 
@@ -28,6 +35,8 @@ public class GameManager : MonoBehaviour
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         scoreText.enabled = false;
+
+        bestScoreText.enabled = false;
     }
 
 
@@ -36,6 +45,9 @@ public class GameManager : MonoBehaviour
         spawner.active = false;
         title.SetActive(true); 
         splash.SetActive(false);
+
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = "Best Score: " + bestScore.ToString();
     }
 
     // Update is called once per frame
@@ -57,18 +69,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        void ResetGame()
-        {
-            spawner.active = true;
-            title.SetActive(false);
-            splash.SetActive(false);
-
-            scoreText.enabled = true;
-            score = 0;
-
-            player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
-            gameStarted = true;
-        }
+        
 
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
 
@@ -94,6 +95,34 @@ public class GameManager : MonoBehaviour
         gameStarted = false;
 
         Invoke("SplashScreen", 2);
+
+        if (score > bestScore)
+        {
+            bestScoreText.color = bestScoreColor;
+
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+            beatBestScore = true;
+            bestScoreText.text = "Best Score: " + bestScore.ToString();
+        }
+    }
+
+    void ResetGame()
+    {
+        bestScoreText.color = bestScoreColor;
+
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+
+        scoreText.enabled = true;
+        score = 0;
+
+        beatBestScore = false;
+        bestScoreText.enabled = true;
+
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
     }
 
     void SplashScreen()
